7a549a1dcfc8c6b9961d59c3fd4e76a911a5b389
Jeremy Bodokh <jbodokh@octo.com>
2015-10-26 09:48:39 +0100

    use variable of the step

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -7,7 +7,7 @@ THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 content_type=""
 date="$(LC_ALL=C date -u +"%a, %d %b %Y %X %z")"
-md5="$(openssl md5 -binary < "$BITRISE_IPA_PATH" | base64)"
+md5="$(openssl md5 -binary < "$bitrise_ipa_path" | base64)"
 SIG_PATH_GROUP_KEY_CPATH=`curl -H "Content-Type: application/json" -X GET --data '{"store_id":"'"$store_id"'", "md5":"'"$md5"'", "date":"'"$date"'", "group_ids":"'"$GROUP"'" }' $APPALOOSA_SERVER/$store_id/bitrise_binaries/get_signature?api_key=$appaloosa_api_key`
 
 echo $SIG_PATH_GROUP_KEY_CPATH > filejson
@@ -27,7 +27,7 @@ if [[ -n "$GROUP" ]];then
   exit 1
 fi
 
-curl -T $BITRISE_IPA_PATH $path \
+curl -T $bitrise_ipa_path $path \
     -H "Date: $date" \
     -H "Authorization: AWS $access_key:$sig" \
     -H "Content-Type: $content_type" \
