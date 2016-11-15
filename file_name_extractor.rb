de316f54a73e531e33a3780e6f53f8d6acf02f15
Christophe Valentin <lv.beleck@gmail.com>
2016-11-15 10:01:38 +0100

    133830053 use File basename function instead of custom regex to determine binary file name

--- file_name_extractor.rb
+++ file_name_extractor.rb
@@ -1,18 +1,20 @@
 #!/usr/bin/env ruby
 require 'json'
 class FilenameExtractor
-	class << self
-		def process!
-			binary_path = ARGV.first
-			raise '[ERROR] $bitrise_ipa_path is empty' if binary_path.empty?
-			puts JSON.dump(filename_from_path(binary_path))
-		end
+  class << self
+    SUPPORTED_EXTENSIONS = %w(.ipa .apk)
 
-		def filename_from_path(path)
-			match = path.match(/.+\/(.+\.(ipa|apk))$/)
-			raise "[ERROR] could not extract filename from #{path}" unless match
-			match[1]
-		end
-	end
+    def process!
+      binary_path = ARGV.first
+      raise '[ERROR] $bitrise_ipa_path is empty' if binary_path.empty?
+      puts JSON.dump(filename_from_path(binary_path))
+    end
+
+    def filename_from_path(path)
+      extname = File.extname(path)
+      raise "[ERROR] extension is not supported: #{extname}" unless SUPPORTED_EXTENSIONS.include?(extname)
+      File.basename(path)
+    end
+  end
 end
 FilenameExtractor.process!
